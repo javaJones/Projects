3a1bd326b2e417fdba37232877de6bef0101428d
javaJones <jamison.jones@icloud.com>
2015-10-01 13:39:12 -0400

    Touch Calculator
    Android Application

---
+++ TouchCalculator/gen/edu/tctc/jjones53/TouchCalculate/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.tctc.jjones53.TouchCalculate;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TouchCalculator/src/edu/tctc/jjones53/TouchCalculator/KeypadButtonCategory.java
@@ -0,0 +1,11 @@
+package edu.tctc.jjones53.TouchCalculator;
+
+public enum KeypadButtonCategory {
+	MEMORYBUFFER
+	, NUMBER
+	, OPERATOR
+	, DUMMY
+	, CLEAR
+	, RESULT
+	, OTHER
+}
