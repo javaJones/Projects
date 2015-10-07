cc24b943d52af66cf18c75008499e66dc1d591e6
javaJones <jamison.jones@icloud.com>
2015-10-07 17:27:55 -0400

    Bill Splitting App
    Android Application

---
+++ SplitTheBill/src/edu/tctc/jjones53/splitthebill/Split.java
@@ -0,0 +1,13 @@
+package edu.tctc.jjones53.splitthebill;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Split extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.split);
+	}
+}
